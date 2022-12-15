e94427bee79e913c19dea2ef80bb94233682cd5a
Ace_Radom <sichenradomlyu@gmail.com>
2022-12-15 09:45:26 +0100

    add pyapi encapsulation header

---
+++ include/PyAPI_Encapsulation.h
@@ -0,0 +1,28 @@
+#ifndef _PYAPI_ENCAPSULATION_H_
+#define _PYAPI_ENCAPSULATION_H_
+
+#include<Python.h>
+#include<string>
+
+#if PY_MAJOR_VERSION < 3
+
+    #define PyImport_Module( module ) PyObject* Py_##module = PyImport_Import( PyString_FromString( #module ) )
+
+#else
+
+    #define PyImport_Module( module ) PyObject* Py_##module = PyImport_Import( PyUnicode_FromString( #module ) )
+
+#endif
+
+#define PyModule_AppendPath( path ) \
+{ \
+    PyRun_SimpleString( "import sys" ); \
+    PyRun_SimpleString( "sys.path.append( '" path "')" ); \
+}
+
+#define PyImport_Function( module , function ) PyObject* PyFunc_##function = PyObject_GetAttrString( module , #function )
+#define PyFunction_Callable( function )                         \
+        ( function && PyCallable_Check( function ) ) ? true     \
+                                                     : false
+
+#endif
